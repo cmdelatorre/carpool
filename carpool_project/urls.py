dde2224f5a5c8dfb1d5dcaadd7416e49e82ccca4
Carlos de la Torre <cmdelatorre@gmail.com>
2018-11-14 13:24:46 -0300

    improved reporting

--- carpool_project/urls.py
+++ carpool_project/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from trips.admin import admin_site
 
-urlpatterns = [path("admin/", admin.site.urls)]
+
+urlpatterns = [
+    path("admin/", admin_site.urls)
+]
--- trips/admin.py
+++ trips/admin.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from decimal import Decimal
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+
 from django.http import HttpResponse
 
 from trips.models import Car, Trip
@@ -8,12 +10,55 @@ from trips.models import Car, Trip
 
 
 
+class TripsAdminSite(AdminSite):
+    site_header = "Los pibe' del Norte - Carpooling"
+    site_title = "Carpool administration"
+    site_url = None
+
+
+admin_site = TripsAdminSite(name='myadmin')
+
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('owner', 'description', 'price_per_trip')
 
 
-admin.site.register(Car, CarAdmin)
+admin_site.register(Car, CarAdmin)
 
+
+
+def solve_collectors_and_payers(payments):
+    """
+    Solve the issue when a person must pay to someone (traveled as passenger) but also collect from 
+    the same person (traveled as driver).
+
+    It computes the difference in that case.
+
+    """
+    processed = defaultdict(bool)
+    for transaction in payments.keys():
+        print('orig', transaction, payments[transaction])
+        # A transaction is a tuple (A, B) such that person A has to pay to person B
+        if processed[transaction]:
+            continue   
+        processed[transaction] = True
+
+        reverse = tuple(reversed(transaction))
+        if reverse in payments:
+            processed[reverse] = True  # Flag the reverse transaction so it is NOT processed later
+            # x le paga a y pero y también le paga a x
+            tmp = payments[transaction]
+            payments[transaction] = tmp - payments[reverse]
+            payments[reverse] = payments[reverse] - tmp
+            print('tr', transaction, payments[transaction])
+            print('rev', reverse, payments[reverse])
+
+def create_report(payments, explain):
+    def pays_or_collects(transaction):
+        return 'paga a' if payments[transaction] > 0 else 'cobra de'
+
+    transactions = [f"{k[0]} {pays_or_collects(k)} {k[1]}: ${abs(payments[k])} por {explain[k]}" for k in sorted(payments)]
+    return "\n".join(transactions)
 
 def compute_payments(queryset):
     
@@ -35,13 +80,15 @@ def compute_payments(queryset):
 
     for trip in queryset:
         for passenger in trip.passengers.exclude(id=trip.car.owner.id):
-            key = (passenger.username, trip.car.owner.username)
+            key = (passenger.username.capitalize(), trip.car.owner.username.capitalize())
             payments[key] += trip.price_per_passenger
             explain[key].append(f"{trip.TRIP_WAYS[trip.way]} el {str(trip.date)}")
     
-    text = "\n".join(
-        f"{k[0]} paga a {k[1]}: {payments[k]} por {explain[k]}" for k in sorted(payments)
-    )
+    print(payments)
+    solve_collectors_and_payers(payments)
+    print(payments)
+
+    text = create_report(payments, explain)
     
     return HttpResponse(text, content_type="text/plain")
 
@@ -60,4 +107,4 @@ class TripAdmin(admin.ModelAdmin):
 
 
 
-admin.site.register(Trip, TripAdmin)
+admin_site.register(Trip, TripAdmin)
